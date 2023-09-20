7e3e7c67a504bc23ed3752d4e32b0606a25e53b5
Tumwesigye Ronnie <59010748+Flash-R@users.noreply.github.com>
2023-09-21 01:39:15 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	let len = str.length; //size of string
+
+	let freq = new array(256).fill(0); //frequency array with 0 at first
+
+	for(let i = 0; i < len i++){
+		fred[s[i].charCodeAt(0)]++;
+	}
+
+	for(let i = 0; i < len; i++){
+		if(fred[s[i].charCodeAt(0)] > 0 && fred[s[i].charCodeAt(0)] == 1){
+			return str.charAt(i);
+		}
+	}
+
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
